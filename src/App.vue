6a9e774c0457f76a8014154852c9e7a083ae740f
Polvere243 <danielercoli@gmail.com>
2024-08-24 15:45:11 +0200

    capied and pasted the router from an old project

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@ export default {
 
 <template>
     <AppHeader />
+    <!-- pagine -->
+    <RouterView />
 </template>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
